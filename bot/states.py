1c4bc5727a95cff940aa9c79eacf7238afbd3997
Aleksandr Rodio <rodionovlead@gmail.com>
2025-07-12 12:06:15 +0300

    Update states.py

--- bot/states.py
+++ bot/states.py
@@ -1,1 +1,8 @@
-# Здесь будут состояния FSM
+from aiogram.fsm.state import StatesGroup, State
+
+class OrderStates(StatesGroup):
+    waiting_for_description = State()
+    waiting_for_address = State()
+    waiting_for_phone = State()
+    waiting_for_time = State()  # новое состояние
+    waiting_for_confirmation = State()
