b1c2e41af7c03d38c7f870134c920e98812ba301
Aleksandr Rodio <rodionovlead@gmail.com>
2025-07-12 12:16:16 +0300

    Update states.py

--- bot/states.py
+++ bot/states.py
@@ -4,5 +4,5 @@ class OrderStates(StatesGroup):
     waiting_for_description = State()
     waiting_for_address = State()
     waiting_for_phone = State()
-    waiting_for_time = State()  # новое состояние
+    waiting_for_time = State()
     waiting_for_confirmation = State()
