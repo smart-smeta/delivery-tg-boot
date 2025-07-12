981b58996f2778246c220a78586ea05a64876623
Aleksandr Rodio <rodionovlead@gmail.com>
2025-07-12 12:17:41 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -3,14 +3,19 @@ import logging
 import os
 
 from aiogram import Bot, Dispatcher
-from aiogram.filters.command import Command
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import Command
+
 from dotenv import load_dotenv
 
-from bot.commands import start_handler
+from bot.commands import (
+    start_handler, profile_handler, help_handler, users_handler,
+    order_start, order_description, order_address, order_phone, order_time, order_confirmation
+)
+from bot.states import OrderStates
 
 load_dotenv()
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-
 logging.basicConfig(level=logging.INFO)
 
 async def main():
@@ -18,11 +23,21 @@ async def main():
         raise ValueError("Не задан токен бота. Установите переменную окружения TELEGRAM_BOT_TOKEN")
 
     bot = Bot(token=TOKEN)
-    dp = Dispatcher()
+    dp = Dispatcher(storage=MemoryStorage())
 
+    # Регистрация хендлеров
     dp.message.register(start_handler, Command("start"))
     dp.message.register(profile_handler, Command("profile"))
     dp.message.register(help_handler, Command("help"))
+    dp.message.register(users_handler, Command("users"))
+
+    # FSM order
+    dp.message.register(order_start, Command("order"))
+    dp.message.register(order_description, OrderStates.waiting_for_description)
+    dp.message.register(order_address, OrderStates.waiting_for_address)
+    dp.message.register(order_phone, OrderStates.waiting_for_phone)
+    dp.message.register(order_time, OrderStates.waiting_for_time)
+    dp.message.register(order_confirmation, OrderStates.waiting_for_confirmation)
 
     await dp.start_polling(bot)
 
